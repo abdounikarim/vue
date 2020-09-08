ddce1b7f3c24d155138bfdcfc2a2c98c215401f0
abdounikarim <abdounikarim@gmail.com>
2020-09-08 14:22:18 +0200

    Attribute Binding

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'My wonderful description',
+        image: "./assets/vmSocks-green.jpg",
+        link: 'https://vuejs.org/v2/guide/',
     }
 })
